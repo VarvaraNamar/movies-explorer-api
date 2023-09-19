6acb965b0dbd728d85092414496576638df6fdc9
Varvara Namar <varvaranamar@yandex.ru>
2023-09-19 09:39:10 +0300

    "mongo"

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 // Секретный ключ для разработки и отладки приложения:
 const SECRET_KEY_DEVELOPMENT = 'development-secret-key';
 
-const { URL_DB = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+const { URL_DB = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 module.exports = {
   SECRET_KEY_DEVELOPMENT,
